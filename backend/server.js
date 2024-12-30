8cb59c4e273eab1abdbc417c7e4c379da3f06a3b
tawiah <tawiahin4k@gmail.com>
2024-12-30 11:05:50 +0000

    implement backend authentication with Express, MongoDB, and dotenv configuration

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,18 @@
-console.log('Server is running on port 3000');
+/** @format */
+
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.Routes.js";
+import connectMongoDB from "./db/connectMongoDB.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use("/api/auth", authRoutes);
+
+app.listen(8000, () => {
+  console.log(`Server is running on port ${PORT}`);
+  connectMongoDB();
+});
