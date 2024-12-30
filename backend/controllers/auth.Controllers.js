01b0ee8a19f97c492846391902d4a0ae9220e98f
tawiah <tawiahin4k@gmail.com>
2024-12-30 15:33:07 +0000

    implement user authentication with signup, login, logout, and profile retrieval; add JWT token generation and cookie handling

--- backend/controllers/auth.Controllers.js
+++ backend/controllers/auth.Controllers.js
@@ -1,19 +1,136 @@
 /** @format */
 
+import bcrypt from "bcryptjs";
+import User from "../models/user.Model.js";
+import { generateTokenAndSetCookie } from "../lib/utils/generateToken.js";
+
 export const signup = async (req, res) => {
-  res.json({
-    data: "You hit the signup endpoint",
-  });
+  try {
+    const { username, fullName, email, password } = req.body;
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({
+        message: "Invalid Email format",
+      });
+    }
+
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return res.status(400).json({
+        message: "Username is already taken",
+      });
+    }
+
+    const existingEmail = await User.findOne({ email });
+    if (existingEmail) {
+      return res.status(400).json({
+        message: "Email is already taken",
+      });
+    }
+
+    // hash the password
+    if (password.length < 6) {
+      return res.status(400).json({
+        message: "Password must be at least 6 characters long",
+      });
+    }
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      fullName,
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    if (newUser) {
+      generateTokenAndSetCookie(newUser._id, res);
+      await newUser.save();
+
+      res.status(201).json({
+        _id: newUser._id,
+        username: newUser.username,
+        email: newUser.email,
+        fullName: newUser.fullName,
+        followers: newUser.followers,
+        following: newUser.following,
+        profileImg: newUser.profileImg,
+        coverImg: newUser.coverImg,
+        bio: newUser.bio,
+      });
+    } else {
+      res.status(400).json({
+        message: "Invalid user data",
+      });
+    }
+  } catch (error) {
+    console.log("Error in signup controller", error.message);
+
+    res.status(500).json({
+      message: "Internal server error",
+    });
+  }
 };
 
 export const login = async (req, res) => {
-  res.json({
-    data: "You hit the login endpoint",
-  });
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    const isPasswordCorrect =
+      user && (await bcrypt.compare(password, user?.password || ""));
+
+    if (!user || !isPasswordCorrect) {
+      return res.status(400).json({
+        message: "Invalid username or password",
+      });
+    }
+
+    generateTokenAndSetCookie(user._id, res);
+
+    res.status(200).json({
+      _id: user._id,
+      username: user.username,
+      email: user.email,
+      fullName: user.fullName,
+      followers: user.followers,
+      following: user.following,
+      profileImg: user.profileImg,
+      coverImg: user.coverImg,
+      bio: user.bio,
+    });
+  } catch (error) {
+    console.log("Error in login controller", error.message);
+
+    res.status(500).json({
+      message: "Internal server error",
+    });
+  }
 };
 
 export const logout = async (req, res) => {
-  res.json({
-    data: "You hit the logout endpoint",
-  });
+  try {
+    res.cookie("jwt", "", { maxAge: 0 });
+    res.status(200).json({
+      message: "Logged out successfully",
+    });
+  } catch (error) {
+    console.log("Error in logout controller", error.message);
+
+    res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+};
+
+export const getMe = async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id).select("-password");
+    res.status(200).json(user);
+  } catch (error) {
+    console.log("Error in getMe controller", error.message);
+
+    res.status(500).json({ message: "Internal server error" });
+  }
 };
--- backend/server.js
+++ backend/server.js
@@ -4,15 +4,20 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.Routes.js";
 import connectMongoDB from "./db/connectMongoDB.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
 
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(cookieParser());
+
 app.use("/api/auth", authRoutes);
 
-app.listen(8000, () => {
+app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   connectMongoDB();
 });
